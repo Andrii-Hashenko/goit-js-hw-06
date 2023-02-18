15e86bb90feefe00a91202b93bebd15f5d6d16e8
Andrii Hashenko <hashenkoaa@gmail.com>
2023-02-18 20:53:21 +0200

    update selector task 1

--- js/task-01.js
+++ js/task-01.js
@@ -4,10 +4,9 @@ console.log("Number of categories: ", count);
 for (let i = 0; i < count; i++) {
   const category = document
     .querySelectorAll(".item")
-    [i].querySelector("h2").textContent;
+    [i].firstElementChild.textContent;
   console.log("Category: ", category);
-  const counts = document
-    .querySelectorAll(".item")
-    [i].querySelectorAll("li").length;
+  const counts =
+    document.querySelectorAll(".item")[i].lastElementChild.children.length;
   console.log("Elements: ", counts);
 }
